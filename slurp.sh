7e816284b566cc6a8f0f932eaeb3c4d366330010
Jason R. Clark <jclark@newrelic.com>
2014-10-27 09:01:05 -0700

    Auto-push to github

--- slurp.sh
+++ slurp.sh
@@ -1,5 +1,9 @@
 if [ -d "/Volumes/GARMIN/Garmin/Activities" ]; then
   if [ -d "$HOME/Rides" ]; then
     mv /Volumes/GARMIN/Garmin/Activities/*.fit $HOME/Rides
+    cd $HOME/Rides
+    git add --all
+    git commit -m "Ride upload"
+    git push origin master
   fi
 fi
